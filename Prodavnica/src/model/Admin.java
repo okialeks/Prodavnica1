985fefe95a7cd14ecaa2a8eb31e59b82eb8c6c9d
Stojan Trajkovic <stojanphp@gmail.com>
2019-04-15 20:03:16 +0200

    model delom sredjen

---
+++ Prodavnica/src/model/Admin.java
@@ -0,0 +1,17 @@
+package model;
+
+public class Admin {
+	
+	final String adminUserName = "admin";
+	final String adminPassword = "admin";
+	
+	public String getAdminUserName() {
+		return adminUserName;
+	}
+	public String getAdminPassword() {
+		return adminPassword;
+	}
+	
+	
+
+}
---
+++ Prodavnica/src/model/User.java
@@ -0,0 +1,64 @@
+package model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int idUser;
+	private String userName;
+	private String password;
+	private double balance;
+	
+	private List<Product> listOfProducts = new ArrayList<Product>();
+
+	public int getIdUser() {
+		return idUser;
+	}
+
+	public void setIdUser(int idUser) {
+		this.idUser = idUser;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public double getBalance() {
+		return balance;
+	}
+
+	public void setBalance(double balance) {
+		this.balance = balance;
+	}
+
+	public List<Product> getListOfProducts() {
+		return listOfProducts;
+	}
+
+	public void setListOfProducts(List<Product> listOfProducts) {
+		this.listOfProducts = listOfProducts;
+	}
+	
+	
+}
